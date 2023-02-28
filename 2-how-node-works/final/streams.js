46b8dc91658fa5867f4a750cab398c9304b6b0f0
anandsundaresan <32130378+anandsundaresan@users.noreply.github.com>
2023-02-28 14:43:50 +0530

    Update streams.js

--- 2-how-node-works/final/streams.js
+++ 2-how-node-works/final/streams.js
@@ -31,3 +31,26 @@ server.on("request", (req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening...");
 });
+
+  // Solution 1 // Entire file i memory
+  //   fs.readFile("./test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  // Solution 2 // Uses streams of read and write
+  //   const readStream = fs.createReadStream("./test-file.txt"); // Read stream, reads in chunks
+  //   readStream.on("data", (chunk) => {
+  //     res.write(chunk); // Write stream, writes in chunks
+  //   });
+  //   readStream.on("end", () => {
+  //     res.end();
+  //   });
+  //   readStream.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+  // Solution 3 // Use pipe function to avoid back preassure which happens when write stream
+  // cannot cop up with rate at which data is read
+  // const readStream = fs.createReadStream("./test-file.txt");
+  //readStream.pipe(res); 
